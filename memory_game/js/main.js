089e79852422959cbe7154fdc7877a74cdcbcce4
Jerem <3j.techno@gmail.com>
2018-11-08 14:59:29 +0100

    Add Condition Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and Runing");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
+var cardInPlay = [];
 
+var cardOne = cards[0];
+cardInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+var cardTwo = cards[1];
+cardInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardInPlay.length === 2) {
+  if (cardOne === cardTwo){
+    alert("You found a match!")
+  }else{
+    alert("Sorry, try again.")
+  }
+}
