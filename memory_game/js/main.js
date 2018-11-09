d895b5c673749687c5b895db060c04d22468bee6
Jerem <3j.techno@gmail.com>
2018-11-09 10:27:57 +0100

    Add function logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,21 @@ console.log("Up and Runing");
 var cards = ["queen", "queen", "king", "king"];
 var cardInPlay = [];
 
-var cardOne = cards[0];
-cardInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-
-var cardTwo = cards[1];
-cardInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardInPlay.length === 2) {
-  if (cardOne === cardTwo){
-    alert("You found a match!")
-  }else{
-    alert("Sorry, try again.")
+var checkForMatch = function(){
+  if (cardInPlay.length === 2) {
+    if (cardInPlay[0] === cardInPlay[1]){
+      alert("You found a match!");
+    }else{
+      alert("Sorry, try again.");
+    }
   }
 }
+var flipCard = function(cardId){
+
+  console.log("User flipped " + cards[cardId]);
+  cardInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
