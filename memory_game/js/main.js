04dd79273483e9696f6be969944b282f05f80df3
Jerem <3j.techno@gmail.com>
2018-11-09 20:00:21 +0100

    Add Object Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and Runing");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardInPlay = [];
 
 var checkForMatch = function(){
@@ -14,8 +35,10 @@ var checkForMatch = function(){
 }
 var flipCard = function(cardId){
 
-  console.log("User flipped " + cards[cardId]);
-  cardInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+  cardInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 
