c25109506602ed48fef3a75176e6e00aea24de65
Jerem <3j.techno@gmail.com>
2018-11-13 16:25:43 +0100

    add shuffle fct & fix alert issue

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,7 +6,7 @@ var cards = [
   },
   {
     rank: "queen",
-    suit: "Diamonds",
+    suit: "diamonds",
     cardImage: "images/queen-of-diamonds.png"
   },
   {
@@ -16,14 +16,16 @@ var cards = [
   },
   {
     rank: "king",
-    suit: "Diamonds",
+    suit: "diamonds",
     cardImage: "images/king-of-diamonds.png"
   }
 ];
+
 var cardInPlay = [];
 
 var createboard = function(){
   cardInPlay = [];
+  shuffleCards();
   for (var i = 0; i < cards.length; i++) {
       var cardElement = document.createElement('img');
       cardElement.setAttribute('src', 'images/back.png');
@@ -33,10 +35,27 @@ var createboard = function(){
   }
 }
 
-var increaseScore = function(){
-  var currentScore = document.getElementById('score').textContent;
-  var newScore = parseInt(currentScore) + 1;
-  document.getElementById('score').textContent = newScore;
+var shuffleCards = function(){
+  //Move every cards one by one to a new position
+  for (var i = 0; i < cards.length; i += 1){
+    var cardToSwitch = cards[i];
+    var newPosition = Math.floor((Math.random()*4)+0);
+    //Switch current card => new position and new position cards => current position
+    cards[i] = cards[newPosition]
+    cards[newPosition] = cardToSwitch;
+  }
+}
+
+var flipCard = function(){
+  console.log(cards)
+  var cardId = this.getAttribute('data-id')
+  cardInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage)
+  if (cardInPlay.length === 2) {
+    //Here we delay the call to this function to allow
+    //the second card to be displayed before the alert pop-up
+    setTimeout(function(){checkForMatch();},500);
+  }
 }
 
 var checkForMatch = function(){
@@ -52,18 +71,16 @@ var checkForMatch = function(){
     }
 }
 
-var flipCard = function(){
-  var cardId = this.getAttribute('data-id')
-  cardInPlay.push(cards[cardId].rank);
-  this.setAttribute('src', cards[cardId].cardImage)
-  if (cardInPlay.length === 2) {
-    checkForMatch();
-  }
+var increaseScore = function(){
+  var currentScore = document.getElementById('score').textContent;
+  var newScore = parseInt(currentScore) + 1;
+  document.getElementById('score').textContent = newScore;
 }
 
 var resetBoard = function(){
+  //Remove all cards elements from the board
   for (var i = 0; i < 4; i++){
-    cardsToRemove = document.getElementById('game-board');
+    let cardsToRemove = document.getElementById('game-board');
     cardsToRemove.removeChild(cardsToRemove.firstChild);
   }
 }
